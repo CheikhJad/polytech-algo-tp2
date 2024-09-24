551927c9d884efb77be12ed38d6b755f9b527bff
JadChekh <cheikhjad@orange.fr>
2024-09-24 13:57:51 +0200

    implémentation de notre premier programme "def is_prime"

--- 3.2 Implémentation.py
+++ 3.2 Implémentation.py
@@ -3,3 +3,17 @@
 a = int(input("Enter a borne positive: "))
 b = int(input("Enter b borne positive: "))
 
+def is_prime(x):
+    res = True
+
+    if x == 2:
+        res = True
+
+    if x <= 1 :
+        res = False
+
+    for i in range(2,x):
+        if x % i == 0 :
+            res = False
+
+    return res
